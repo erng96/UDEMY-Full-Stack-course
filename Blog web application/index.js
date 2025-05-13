fd9763f4fc2c5518658fb4dfb6993b94b287e559
ernesto <thenewage2018@gmail.com>
2025-05-13 13:15:15 -0600

    add a link to post element to redirect useres to the edit post page and added css properties to help useres I.D. the link.

--- Blog web application/index.js
+++ Blog web application/index.js
@@ -7,7 +7,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 const posts = [];
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { posts: posts });
 });
 app.get("/about", (req, res) => {
   res.render("about.ejs");
@@ -24,8 +24,8 @@ app.post("/createpost", (req, res) => {
     content: req.body.content,
     postId: Math.floor(Math.random() * 1000),
   };
-  console.log(post);
   posts.push(post);
+  res.redirect("/posts");
 });
 app.post("/deletepost", (req, res) => {
   // the postId gets the value property from targeting the input with the name property: Id
@@ -37,6 +37,28 @@ app.post("/deletepost", (req, res) => {
     console.log("Post not found");
   }
   res.redirect("/posts");
+});
+app.post("/editpost", (req, res) => {
+  const postId = req.body.id;
+  const postIndex = posts.findIndex((post) => post.postId == postId);
+  if (postIndex !== -1) {
+    const post = posts[postIndex];
+    res.render("editpost.ejs", { post: post });
+    console.log(post);
+  }
+});
+app.post("/updatepost", (req, res) => {
+  const editedpost = {
+    title: req.body.title,
+    content: req.body.content,
+    postId: req.body.id,
+  };
+  const postIndex = posts.findIndex((post) => post.postId == editedpost.postId);
+
+  if (postIndex !== -1) {
+    posts[postIndex] = editedpost;
+    res.redirect("/posts");
+  }
 });
 app.get("/posts", (req, res) => {
   res.render("posts.ejs", { posts: posts });
